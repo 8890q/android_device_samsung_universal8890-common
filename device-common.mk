e2c03a137d8fe9459e9474dcdd8a1a40b4592327
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-24 20:55:55 -0700

    universal8890: Add netutils-wrapper impl
    Change-Id: Id6f168c7b8a30ee6dc368b12ca0521ad173f24f4

--- device-common.mk
+++ device-common.mk
@@ -165,6 +165,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl
 
+# Network
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # NFC
 PRODUCT_PACKAGES += \
     libnfc-nci \
