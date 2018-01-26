6f45bbd64f054cf820016ab30098c466a6092aef
Bruno Martins <bgcngm@gmail.com>
2018-01-26 11:27:35 +0000

    universal8890: Binderize them all
     * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
    Change-Id: Id50291488d655187aa013c51bdd6890dca010564

--- device-common.mk
+++ device-common.mk
@@ -132,6 +132,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1
 
 # Health
@@ -185,7 +186,8 @@ PRODUCT_COPY_FILES += \
 
 # Memory
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # Network
 PRODUCT_PACKAGES += \
