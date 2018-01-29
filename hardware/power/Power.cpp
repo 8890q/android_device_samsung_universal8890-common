325832930d626e351be8a36614a1ae208addf270
Simon Shields <simon@lineageos.org>
2018-01-29 17:08:32 +1100

    universal8890: add support for AOD DT2W
       * Samsung touchscreens support using double-tap to wake to trigger
         always on display. Implement support for the sysfs API used to
         en/disable this. Note that this requires a kernel change to send
         KEY_POWER on the dt2w event instead of KEY_BLACK_UI_GESTURE - e.g.
         https://review.lineageos.org/c/LineageOS/android_kernel_samsung_universal8890/+/301195
    
       * [fcuzzocrea]: to simplify the logic of the original commit, another
         kernel change is needed :
         https://review.lineageos.org/c/LineageOS/android_kernel_samsung_universal8890/+/301231
         in this way, the AOD rectangle will not be cleared everytime the
         screen will be switched on, so it is not needed to set the
         rectangle everytime.
         Clearing the rectangle is something which is useful only on Samsung
         based ROMs which supports indeed different kinds of gestures when
         coupled with Samsung's smart cover.
         The only thing we support on AOSP is DT2W so it is safe to set the
         AOD rectangle as the whole panel area once and keep it.
    
       * Remove some white spaces while we are at it
    
    Change-Id: I80898c8810216a90cc11b4e8b0f12a1918cd17a3

--- hardware/power/Power.cpp
+++ hardware/power/Power.cpp
@@ -71,7 +71,7 @@ out:
     set("/sys/power/cpuhotplug/max_online_cpu", interactive ? "8" : "6");
 
     setProfile(interactive ? PowerProfile::POWER_SAVE : PowerProfile::BALANCED);
-    
+
     return Void();
 }
 
@@ -108,16 +108,23 @@ Return<void> Power::powerHint(PowerHint hint, int32_t data) {
     return Void();
 }
 
-Return<void> Power::setFeature(Feature feature __unused, bool activate __unused) {
+Return<void> Power::setFeature(Feature feature, bool activate) {
     if (!initialized) {
         initialize();
     }
 
-#ifdef TAP_TO_WAKE_NODE
     if (feature == Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE) {
-        set(TAP_TO_WAKE_NODE, activate ? "1" : "0");
+        if (activate) {
+            std::string screen_size = get<std::string>("/sys/class/graphics/fb0/virtual_size", "");
+            if (!screen_size.empty()) {
+                set("/sys/class/sec/tsp/cmd", "aod_enable,1");
+                set("/sys/class/sec/tsp/cmd", "set_aod_rect," + screen_size + ",0,0");
+            }
+        } else {
+            set("/sys/class/sec/tsp/cmd", "aod_enable,0");
+            set("/sys/class/sec/tsp/cmd", "set_aod_rect,0,0,0,0");
+        }
     }
-#endif
 
     return Void();
 }
@@ -156,7 +163,7 @@ void Power::initialize() {
     set(cpuInteractivePaths.at(1) + "/timer_rate", "20000");
     set(cpuInteractivePaths.at(1) + "/timer_slack", "20000");
     set(cpuInteractivePaths.at(1) + "/min_sample_time", "40000");
-    set(cpuInteractivePaths.at(1) + "/boostpulse_duration", "40000");  
+    set(cpuInteractivePaths.at(1) + "/boostpulse_duration", "40000");
 
     initialized = true;
 }
