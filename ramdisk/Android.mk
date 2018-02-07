35983990daf4f82de4ead446bda7708210716520
Bruno Martins <bgcngm@gmail.com>
2018-02-07 22:47:01 +0000

    universal8890: Move fstab to /vendor
    Change-Id: Ie1c6cb662f2f54c7c14d3e7d55aedbfbeef5f8a7

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE		:= fstab.samsungexynos8890
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES     := etc/fstab.samsungexynos8890
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.recovery.samsungexynos8890.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES        := etc/init.recovery.samsungexynos8890.rc
+LOCAL_SRC_FILES     := etc/init.recovery.samsungexynos8890.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
