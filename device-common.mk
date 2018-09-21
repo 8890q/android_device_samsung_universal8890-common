e534c57f308b0d0914ba4a193f08905a6e1e393d
Stricted <info@stricted.net>
2018-09-22 00:19:33 +0200

    universal8890: build renderscript hal
    Change-Id: I3699568130975c997063c51a29b67da972f39e80

--- device-common.mk
+++ device-common.mk
@@ -120,6 +120,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
