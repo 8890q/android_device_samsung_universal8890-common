9a50b880527e303f6878fa0d172f9cc73fd876ec
Stricted <info@stricted.net>
2018-09-22 00:18:46 +0200

    universal8890: build graphic packages
    Change-Id: Ie4d693823e8540546666c5f1144acae8e85b0603

--- device-common.mk
+++ device-common.mk
@@ -73,6 +73,13 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
     android.hardware.configstore@1.0-service
 
+# Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/permissions/com.samsung.permission.HRM_EXT.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.samsung.permission.HRM_EXT.xml \
