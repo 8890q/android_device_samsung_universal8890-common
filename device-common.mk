7b7b06b84e22d381c43c17398515ba04007ef9bf
Stricted <info@stricted.net>
2018-09-22 00:16:54 +0200

    universal8890: build configstore HIDL HAL
    Change-Id: If2d406bff9a2d8ba57c2184e0075ea99204fb141

--- device-common.mk
+++ device-common.mk
@@ -68,6 +68,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/camera/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-impl \
+    android.hardware.configstore@1.0-service
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/permissions/com.samsung.permission.HRM_EXT.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.samsung.permission.HRM_EXT.xml \
