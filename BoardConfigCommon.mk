f58900d08744451f145c5a3f8d4479c4e46812aa
Stricted <info@stricted.net>
2018-09-22 00:09:35 +0200

    universal8890: add common wifi board flags
    also build common wifi packages
    
    Change-Id: Ife1f86334399bfe3e01ed8927bf11748d1758fc4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,5 +88,20 @@ TARGET_BOARD_PLATFORM := exynos5
 TARGET_BOOTLOADER_BOARD_NAME := universal8890
 TARGET_SOC := exynos8890
 
+# Wifi
+TARGET_USES_64_BIT_BCMDHD        := true
+BOARD_HAVE_SAMSUNG_WIFI          := true
+BOARD_WLAN_DEVICE                := bcmdhd
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+WPA_SUPPLICANT_USE_HIDL          := true
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/etc/wifi/bcmdhd_apsta.bin"
+WIFI_BAND                        := 802_11_ABG
+
 # Inherit from the proprietary version
 -include vendor/samsung/universal8890-common/BoardConfigVendor.mk
