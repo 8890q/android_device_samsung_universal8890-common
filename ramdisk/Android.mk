511ea4edc4507b15d97579142efed5f3e4bf01c6
ivanmeler <i_ivan@windowslive.com>
2018-09-22 03:14:19 +0200

    universal8890: import common ramdisk
    Change-Id: I80ddc9b0cfc7fc312513ba795443b294135aaf35

---
+++ ramdisk/Android.mk
@@ -0,0 +1,49 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.samsungexynos8890
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_SRC_FILES        := etc/fstab.samsungexynos8890
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.baseband.rc
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_SRC_FILES 	:= etc/init.baseband.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.samsung.rc
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_SRC_FILES        := etc/init.samsung.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.samsungexynos8890.rc
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_SRC_FILES        := etc/init.samsungexynos8890.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.samsungexynos8890.usb.rc
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_SRC_FILES        := etc/init.samsungexynos8890.usb.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= ueventd.samsungexynos8890.rc
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_SRC_FILES        := etc/ueventd.samsungexynos8890.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
