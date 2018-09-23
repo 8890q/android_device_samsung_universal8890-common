05b8f5bf4125d38a1012f2a8698f793cf10f9fcd
Stricted <info@stricted.net>
2018-09-23 02:46:37 +0200

    universal8890: add common bluetooth configuration and packages
    Change-Id: Ibf15cda6cfd34f44d9aa210369e5bfd9891b5c47

--- device-common.mk
+++ device-common.mk
@@ -57,6 +57,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl.universal8890 \
+    android.hardware.bluetooth@1.0-service.universal8890 \
+    libbt-vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
