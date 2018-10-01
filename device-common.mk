f4b19d0885b1b140e8e501c62dd7e58e99e9714e
Stricted <info@stricted.net>
2018-10-01 11:13:26 +0200

    universal8890: build lights hal from hardware/samsung
    Change-Id: Ib53d6d665658f0c436f87084943950b43d4ac4f5

--- device-common.mk
+++ device-common.mk
@@ -122,6 +122,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     libkeymaster3device
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.samsung
+
 # Media
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs.xml \
