d8b128fcc75d6125f0c30466e888ba7b2ff037a7
Sundong Ahn <sundongahn@google.com>
2018-12-17 16:40:39 +0900

    universal8890: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014

--- system_prop.mk
+++ system_prop.mk
@@ -31,7 +31,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwc.winupdate=1 \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
-    ro.opengles.version=196610
+    ro.opengles.version=196610 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Keystore
 PRODUCT_PROPERTY_OVERRIDES += \
