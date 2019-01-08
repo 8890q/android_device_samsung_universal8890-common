296f1088013e2cb998d419685e5ff9ecd422ac5b
Paul Keith <javelinanddart@gmail.com>
2019-01-09 00:47:48 +0100

    universal8890: Build vendor.lineage.touch HAL from hardware/samsung
    Change-Id: I6eca1e9875cb5793a3a45c6e77bc201946ebd897

--- device-common.mk
+++ device-common.mk
@@ -245,6 +245,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.samsung
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
