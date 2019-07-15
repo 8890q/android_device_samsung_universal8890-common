c3c16739261380e0b67dee42fca47469185761c0
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    universal8890: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,7 +51,6 @@ BOARD_CUSTOM_BT_CONFIG := $(COMMON_PATH)/bluetooth/libbt_vndcfg.txt
 
 # Charger
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
 
