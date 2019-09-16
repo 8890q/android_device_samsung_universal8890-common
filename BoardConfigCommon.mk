c9cc2aa0ee8cc6061eb99fed6f52efa381d953e8
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-16 21:02:36 +0200

    universal8890: Correctly define variables
    Needed after setting soong namespace
    
    Change-Id: Ia1a58bea3df625fd76c060370dc7f9fa2a597765

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,7 +50,7 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_CUSTOM_BT_CONFIG := $(COMMON_PATH)/bluetooth/libbt_vndcfg.txt
 
 # Camera
-TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := libcamera_parameters_8890
+TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := //$(COMMON_PATH):libcamera_parameters_8890
 
 # Charger
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
