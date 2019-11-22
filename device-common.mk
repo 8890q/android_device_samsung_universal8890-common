ce8301697d96b2c5c1a8252c1e1bcfec3695da05
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    universal8890: Switch to Audio HAL V6
    Switch to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8

--- device-common.mk
+++ device-common.mk
@@ -64,9 +64,9 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.r_submix.default \
     audio.primary.universal8890_32 \
-    android.hardware.audio@5.0-impl:32 \
+    android.hardware.audio@6.0-impl:32 \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.effect@2.0-service \
     android.hardware.bluetooth.audio@2.0-impl:32 \
     libtinycompress
