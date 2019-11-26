a639955e858699b9fc5f60478b6be6b1b2e602c4
Robert Shih <robertshih@google.com>
2019-11-25 22:25:42 -0800

    universal8890: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420
    Signed-off-by: Arian <arian.kulmer@web.de>

--- device-common.mk
+++ device-common.mk
@@ -104,7 +104,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:32 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
     
 # DTB tool    
 PRODUCT_HOST_PACKAGES += \
