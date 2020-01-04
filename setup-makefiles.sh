f51077e729af0ae1831e481ae6d24bc34e41cba0
Jan Altensen <info@stricted.net>
2020-01-04 22:03:12 +0100

    universal8890:  copy libGLES_mali.so to vulkan.exynos5.so
    Change-Id: I3b41b9386b0c1a286a896b468e9c39dbd147d46a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -48,7 +48,7 @@ OUTDIR=vendor/$VENDOR/$DEVICE_COMMON
 (cat << EOF) >> $LINEAGE_ROOT/$OUTDIR/Android.mk
 include \$(CLEAR_VARS)
 
-EGL_LIBS := libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.exynos5.so
+EGL_LIBS := libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1
 
 EGL_32_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/,\$(EGL_LIBS))
 \$(EGL_32_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
@@ -64,7 +64,23 @@ EGL_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/,\$(EGL_LIBS))
 	@rm -rf \$@
 	\$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so \$@
 
-ALL_DEFAULT_INSTALLED_MODULES += \$(EGL_32_SYMLINKS) \$(EGL_64_SYMLINKS)
+VULKAN_LIBS := vulkan.exynos5.so
+
+VULKAN_32_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/hw/,\$(VULKAN_LIBS))
+\$(VULKAN_32_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
+	@echo "Copy: Vulkan 32-bit lib: \$@"
+	@mkdir -p \$(dir \$@)
+	@rm -rf \$@
+	\$(hide) cp \$(TARGET_OUT_VENDOR)/lib/egl/libGLES_mali.so \$@
+
+VULKAN_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/hw/,\$(VULKAN_LIBS))
+\$(VULKAN_64_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
+	@echo "Copy: Vulkan 64-bit lib: \$@"
+	@mkdir -p \$(dir \$@)
+	@rm -rf \$@
+	\$(hide) cp \$(TARGET_OUT_VENDOR)/lib64/egl/libGLES_mali.so \$@
+
+ALL_DEFAULT_INSTALLED_MODULES += \$(EGL_32_SYMLINKS) \$(EGL_64_SYMLINKS) \$(VULKAN_32_SYMLINKS) \$(VULKAN_64_SYMLINKS)
 
 EOF
 ###################################################################################################
