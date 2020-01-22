e3be245914fc9269f4a29c5973a0ef1306393af6
ivanmeler <i_ivan@windowslive.com>
2020-01-22 17:35:26 +0000

    universal8890: build only 32bit audio hal

--- device-common.mk
+++ device-common.mk
@@ -38,7 +38,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
-    audio.primary.universal8890 \
+    audio.primary.universal8890_32 \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
