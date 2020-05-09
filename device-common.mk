ffe5a6594b78025e7f4e2f2c7b2b70eba9667ed9
Willi Ye <williye97@gmail.com>
2020-05-10 00:15:07 +0800

    universal8890: build sensors implementation from hw\samsung
    * Get rid of com.samsung.permission.SSENSOR
    * Map vendor proximity sensor type to generic
    
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- device-common.mk
+++ device-common.mk
@@ -226,6 +226,11 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/ril/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml \
     $(COMMON_PATH)/configs/ril/spn-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/spn-conf.xml
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl.samsung \
+    android.hardware.sensors@1.0-service
+
 # Shims
 PRODUCT_PACKAGES += \
     libexynoscamera_shim
