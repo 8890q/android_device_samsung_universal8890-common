a20a434a47f0c6bcb23b5af30de8dbab849418d6
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-06-29 14:28:02 +0200

    universal8890: build thermal hidl

--- device-common.mk
+++ device-common.mk
@@ -228,6 +228,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
