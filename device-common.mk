58c6f107f671891f6bf439b3a259e1474c5331ea
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-07-06 23:30:40 +0200

    universal8890: import prebuilt camera provider and dependencies

--- device-common.mk
+++ device-common.mk
@@ -80,10 +80,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl \
     camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    camera.device@3.3-impl \
+    camera.device@3.4-impl \
     Snap
 
 # Configstore
