41d4822414d992cb7592411930f72620020394f9
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-08-28 09:27:26 +0200

    universal8890: power: rebrand to universal8890
    Change-Id: I35b0c08e087e34f09367bd523e54bfad4f1934a9

--- device-common.mk
+++ device-common.mk
@@ -267,7 +267,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.exynos
+    android.hardware.power@1.0-service.universal8890
 
 # Ramdisk
 PRODUCT_PACKAGES += \
