1aa86bdb38f17b2099f13489497403d1bd9e52db
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-01-26 19:37:09 +0100

    universal8890: build hwcomposer and memtrack hals from source
    Change-Id: Ibd0e95ee94d4bc56ba9e6d13a089184d2db2ad4c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -215,11 +215,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 # Shims
 TARGET_LD_SHIM_LIBS += \
     /vendor/lib64/libexynoscamera.so|/vendor/lib64/libexynoscamera_shim.so \
-    /vendor/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
-    /vendor/lib64/libexynosdisplay.so|/vendor/lib64/libexynosdisplay_shim.so \
-    /vendor/lib/libexynosdisplay.so|/vendor/lib/libexynosdisplay_shim.so \
-    /vendor/lib64/hw/hwcomposer.exynos5.so|/vendor/lib64/libexynosdisplay_shim.so \
-    /vendor/lib/hw/hwcomposer.exynos5.so|/vendor/lib/libexynosdisplay_shim.so
+    /vendor/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
--- device-common.mk
+++ device-common.mk
@@ -134,7 +134,11 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    hwcomposer.exynos5 \
+    libfimg \
+    libhwc2on1adapter \
+    libion
 
 # Health
 PRODUCT_PACKAGES += \
@@ -187,7 +191,8 @@ PRODUCT_COPY_FILES += \
 # Memory
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service
+    android.hardware.memtrack@1.0-service \
+    memtrack.universal8890
 
 # Network
 PRODUCT_PACKAGES += \
@@ -315,8 +320,7 @@ PRODUCT_PACKAGES += \
 
 # Shims
 PRODUCT_PACKAGES += \
-    libexynoscamera_shim \
-    libexynosdisplay_shim
+    libexynoscamera_shim
 
 # Stagefright
 PRODUCT_PACKAGES += \
