53d7bf21021875a9beffd0505717cbfd1bd51df6
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-02-02 19:50:55 +0100

    universal8890: sepolicy: partial rewrite
        * start using common samsung_slsi sepolicy
        * move sepolicy to vendor folder
        * use as much macros as we can
        * maximize usage of regular expressions
    
    Change-Id: Idf93ef76a48457ae33b6e2ad4e8f584906224428

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,7 +165,10 @@ TARGET_USES_VND_SECRIL := true
 BOARD_SECCOMP_POLICY += $(COMMON_PATH)/seccomp
 
 # SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+include device/lineage/sepolicy/exynos/sepolicy.mk
+BOARD_SEPOLICY_TEE_FLAVOR := mobicore
+include device/samsung_slsi/sepolicy/sepolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Shims
 TARGET_LD_SHIM_LIBS += \
