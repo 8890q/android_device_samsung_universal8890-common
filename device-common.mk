f79828a11ae468398289148f15c5cf398cde7b34
Jan Altensen <info@stricted.net>
2021-04-24 02:54:40 +0200

    universal8890: build nfc hal from source
    Change-Id: Ief7ef8f2a597b1a978ea4b0ec4e41391cec03a99

--- device-common.mk
+++ device-common.mk
@@ -210,13 +210,12 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag \
     com.android.nfc_extras \
-    libclang_rt.ubsan_standalone-aarch64-android
+    android.hardware.nfc@1.2-service.samsung
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf \
     $(COMMON_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
-    $(COMMON_PATH)/configs/nfc/nfcee_access.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/nfcee_access.xml \
-    $(COMMON_PATH)/configs/nfc/nfc_key:$(TARGET_COPY_OUT_VENDOR)/etc/nfc_key
+    $(COMMON_PATH)/configs/nfc/nfcee_access.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/nfcee_access.xml
 
 # Offmode charger
 # Use LineageOS images if available, aosp ones otherwise
