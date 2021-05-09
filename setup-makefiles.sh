ccecd144b6929f4f0ba82f20f9f78232816a1110
Jan Altensen <info@stricted.net>
2021-05-09 22:51:57 +0200

    Revert "universal8890: copy libGLES_mali.so to vulkan.exynos5.so"
     * apparently symlinks works again?
    
    This reverts commit 6fecc7c5c8d889496557d520c9a2c7056e22ae58.
    
    [fcuzzocrea] also move the vulkan hal outside hw folder to workaround
    
    E vndksupport: Could not load vulkan.exynos5.so from default namespace: dlopen failed: library "vulkan.exynos5.so" not found.
    
    since commit
    https://github.com/LineageOS/android_frameworks_native/commit/d10af92a7b83d252038944cad6014a00bfa7ca25
    the system tries to dlopen vulkan hal from the default namespace, which
    is not allowed to search for libs in /vendor/{LIB}/hw. Extending the
    default namespace to access lib(64)/hw is more hacky than just putting
    a symlink in lib(64), so go with that route.
    
    Change-Id: I7bee55e415bb30f2fd72bb88480302ef48f72818

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -38,7 +38,7 @@ OUTDIR=vendor/$VENDOR/$DEVICE_COMMON
 (cat << EOF) >> $ANDROID_ROOT/$OUTDIR/Android.mk
 include \$(CLEAR_VARS)
 
-EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1
+EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 vulkan.exynos5.so
 
 EGL_32_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/,\$(EGL_LIBS))
 \$(EGL_32_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
@@ -54,23 +54,7 @@ EGL_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/,\$(EGL_LIBS))
 	@rm -rf \$@
 	\$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so \$@
 
-VULKAN_LIBS := vulkan.exynos5.so
-
-VULKAN_32_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/hw/,\$(VULKAN_LIBS))
-\$(VULKAN_32_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
-	@echo "Copy: Vulkan 32-bit lib: \$@"
-	@mkdir -p \$(dir \$@)
-	@rm -rf \$@
-	\$(hide) cp \$(TARGET_OUT_VENDOR)/lib/egl/libGLES_mali.so \$@
-
-VULKAN_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/hw/,\$(VULKAN_LIBS))
-\$(VULKAN_64_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
-	@echo "Copy: Vulkan 64-bit lib: \$@"
-	@mkdir -p \$(dir \$@)
-	@rm -rf \$@
-	\$(hide) cp \$(TARGET_OUT_VENDOR)/lib64/egl/libGLES_mali.so \$@
-
-ALL_DEFAULT_INSTALLED_MODULES += \$(EGL_32_SYMLINKS) \$(EGL_64_SYMLINKS) \$(VULKAN_32_SYMLINKS) \$(VULKAN_64_SYMLINKS)
+ALL_DEFAULT_INSTALLED_MODULES += \$(EGL_32_SYMLINKS) \$(EGL_64_SYMLINKS)
 
 EOF
 ###################################################################################################
