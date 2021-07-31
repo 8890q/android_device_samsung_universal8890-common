777dd70c9bcabfab6b68edf4fa6495312facb83d
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-07-31 10:20:35 +0200

    universal8890: build batterylifeextender HIDL
    Change-Id: Ia6b711ffc79e261c0fb89d834380fbf1a7bf0f19

--- device-common.mk
+++ device-common.mk
@@ -68,6 +68,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# BatteryLifeExtender
+PRODUCT_PACKAGES += \
+    vendor.lineage.batterylifeextender@1.0-service.samsung
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl:64 \
