a43bc0f7dceb4d85ea343306500300cdbd46b63f
ivanmeler <i_ivan@windowslive.com>
2022-03-23 19:01:00 +0000

    Drop prebuilt samsung camera provider Also uprev source built aosp one to 2.5, because why not

--- device-common.mk
+++ device-common.mk
@@ -78,11 +78,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    camera.device@3.3-impl \
-    camera.device@3.4-impl \
     camera.exynos5 \
+    android.hardware.camera.provider@2.5-service \
     Snap
 
 PRODUCT_COPY_FILES += \
