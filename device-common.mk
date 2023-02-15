43f1dbbf74ab96e53dc8d60f2aa412b79c71c4f6
ivanmeler <i_ivan@windowslive.com>
2023-02-15 13:41:27 +0100

    temp: dont build thermal on note

--- device-common.mk
+++ device-common.mk
@@ -338,9 +338,6 @@ PRODUCT_PACKAGES += \
     libgui_vendor
 
 # Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.samsung
-
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
