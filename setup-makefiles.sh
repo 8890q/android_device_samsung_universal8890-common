6a1d64595347ebf4b9d489bbe8703e48d2f6defd
Ivan Meler <i_ivan@windowslive.com>
2023-02-15 12:47:09 +0100

    Add support for grace and gracer series

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -27,7 +27,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "herolte hero2lte"
+write_headers "herolte hero2lte gracelte graceltekor gracerlte gracerltekor"
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
